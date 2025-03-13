2279b4cf612d56782042f7b8e6bd4c0619b7e464
Neha Sinha <106995582+nehasinh1996@users.noreply.github.com>
2025-03-14 01:27:34 +0530

    Delete backend directory

--- backend/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getProfile } = require("../controllers/profileController");
-const { protect } = require("../middlewares/authMiddleware");
-
-const router = express.Router();
-
-router.get("/", protect, getProfile);
-
-module.exports = router;
--- backend/utils/index.js
+++
@@ -1,5 +0,0 @@
-const jwtUtils = require("./jwtUtils");
-const otpUtils = require("./otpUtils");
-const twilioUtils = require("./twilioUtils");
-
-module.exports = { jwtUtils, otpUtils, twilioUtils };
